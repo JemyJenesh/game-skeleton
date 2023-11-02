443ad02f4d4c2991d0a5d62ebb2cb334bdc330c9
Jemy Jenesh <xemejenesh@gmail.com>
2023-11-02 22:18:28 +0545

    add react app

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "public")));
+}
+
 app.get("/api/status", (req, res) => {
   res.json({ ok: true });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, "build", "public"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./client/public/index.html",
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    proxy: {
+      "/api/**": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
+  },
+};
