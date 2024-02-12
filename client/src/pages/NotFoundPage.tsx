2e3fac400dde185f7f074c1833137ed5564453d2
Jemy Jenesh <xemejenesh@gmail.com>
2024-02-12 19:08:20 +0545

    feat: add home button in 404 page

--- client/src/pages/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage.tsx
@@ -1,6 +1,8 @@
 import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
 import Typography from "@mui/joy/Typography";
 import React from "react";
+import { Link } from "react-router-dom";
 import { PageTransition } from "../components";
 
 export function NotFoundPage() {
@@ -8,7 +10,12 @@ export function NotFoundPage() {
     <PageTransition>
       <Box sx={{ textAlign: "center", p: 2 }}>
         <Typography level="h1">Page not found</Typography>
-        <Typography>The page you are looking for doesn't exist.</Typography>
+        <Typography gutterBottom>
+          The page you are looking for doesn't exist.
+        </Typography>
+        <Button component={Link} to="/">
+          Go Home
+        </Button>
       </Box>
     </PageTransition>
   );
