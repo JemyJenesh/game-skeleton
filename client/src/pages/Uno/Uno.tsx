be488cd61e25ad5274098a76541e8658b616a871
Jemy Jenesh <xemejenesh@gmail.com>
2024-02-13 11:07:32 +0545

    feat: animate direction indicator

--- client/src/pages/Uno/Uno.tsx
+++ client/src/pages/Uno/Uno.tsx
@@ -1,4 +1,3 @@
-import CachedIcon from "@mui/icons-material/Cached";
 import { Box, CircularProgress, Stack, Typography } from "@mui/joy";
 import React, { useEffect } from "react";
 import { Flipped, Flipper } from "react-flip-toolkit";
@@ -305,7 +304,6 @@ function DrawPile() {
 function DiscardPile() {
   const pile = useUnoGameStore((state) => state.pile);
   const direction = useUnoGameStore((state) => state.direction);
-  const deg = direction === 1 ? 0 : 180;
 
   return (
     <Box
@@ -314,10 +312,13 @@ function DiscardPile() {
       <Box
         sx={{
           position: "absolute",
-          transform: `rotateY(${deg}deg)`,
+          animationName: direction === 1 ? "spin" : "spin-reverse",
+          animationDuration: "5s",
+          animationIterationCount: "infinite",
+          animationTimingFunction: "linear",
         }}
       >
-        <CachedIcon sx={{ height: 500, width: 500 }} />
+        <DirectionIndicator direction={direction} />
       </Box>
       {pile?.map((card) => (
         <Flipped key={card._id} flipId={card._id}>
@@ -402,5 +403,43 @@ function PlayersSeat() {
         </Stack>
       ))}
     </Stack>
+  );
+}
+
+function DirectionIndicator({ direction }: { direction: 1 | -1 }) {
+  const deg = direction === 1 ? 0 : 180;
+
+  return (
+    <svg
+      height="256px"
+      width="256px"
+      version="1.1"
+      id="Capa_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 60 60"
+      xmlSpace="preserve"
+      fill="#26B99A"
+      style={{
+        transform: `rotateY(${deg}deg)`,
+      }}
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <g>
+          <path d="M52,31c-0.553,0-1-0.447-1-1c0-11.579-9.42-21-21-21S9,18.421,9,30c0,0.553-0.448,1-1,1 s-1-0.447-1-1C7,17.317,17.318,7,30,7s23,10.317,23,23C53,30.553,52.553,31,52,31z"></path>
+          <path d="M52,31c-0.256,0-0.512-0.098-0.707-0.293c-0.391-0.391-0.391-1.023,0-1.414l7-7 c0.391-0.391,1.023-0.391,1.414,0s0.391,1.023,0,1.414l-7,7C52.512,30.902,52.256,31,52,31z"></path>
+          <path d="M52,31c-0.256,0-0.512-0.098-0.707-0.293l-7-7c-0.391-0.391-0.391-1.023,0-1.414 s1.023-0.391,1.414,0l7,7c0.391,0.391,0.391,1.023,0,1.414C52.512,30.902,52.256,31,52,31z"></path>
+          <path d="M30,53C17.318,53,7,42.683,7,30c0-0.553,0.448-1,1-1s1,0.447,1,1c0,11.579,9.42,21,21,21 s21-9.421,21-21c0-0.553,0.447-1,1-1s1,0.447,1,1C53,42.683,42.682,53,30,53z"></path>
+          <path d="M1,38c-0.256,0-0.512-0.098-0.707-0.293c-0.391-0.391-0.391-1.023,0-1.414l7-7 c0.391-0.391,1.023-0.391,1.414,0s0.391,1.023,0,1.414l-7,7C1.512,37.902,1.256,38,1,38z"></path>
+          <path d="M15,38c-0.256,0-0.512-0.098-0.707-0.293l-7-7c-0.391-0.391-0.391-1.023,0-1.414 s1.023-0.391,1.414,0l7,7c0.391,0.391,0.391,1.023,0,1.414C15.512,37.902,15.256,38,15,38z"></path>
+        </g>
+      </g>
+    </svg>
   );
 }
